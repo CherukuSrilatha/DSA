8c6c5364343a42beaa8487133e4fb0cde32283c2
Cheruku Srilatha <cherukusrilatha1@gmail.com>
2026-06-08 22:29:49 +0530

    Search Insert Position

---
+++ Arrays/Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        ans=-1
+        for i in range(len(nums)):
+            if nums[i]==target:
+                ans=i
+        if ans==-1:
+            for i in range(len(nums)):
+                if nums[i]>target:
+                    return i
+            else:
+                return len(nums)
+
+        else:
+            return ans
