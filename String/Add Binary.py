c4483924a9d50ad502e9b5e1ef21601ea1dfe021
Cheruku Srilatha <cherukusrilatha1@gmail.com>
2026-06-10 06:42:53 +0530

    Add Binary

---
+++ String/Add Binary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x = int(a, 2)
+        y = int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:]
