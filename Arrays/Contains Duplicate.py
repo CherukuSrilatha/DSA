8d3a3fbce308631e21b7b477b288bc3a0f0f3798
Cheruku Srilatha <cherukusrilatha1@gmail.com>
2026-06-19 19:08:32 +0530

    Contains Duplicate

---
+++ Arrays/Contains Duplicate.py
@@ -0,0 +1,9 @@
+from ast import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s=set(nums)
+        if len(nums)==len(s):
+            return False
+        else:
+            return True
+        
