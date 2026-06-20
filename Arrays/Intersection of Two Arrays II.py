7bd50eec3eb21f81fe3d0ebbd304c47e7b3b8335
Cheruku Srilatha <cherukusrilatha1@gmail.com>
2026-06-20 12:28:52 +0530

    Intersection of Two Arrays II

---
+++ Arrays/Intersection of Two Arrays II.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l=[]
+        for i in nums1:
+            if i in nums2:
+                l.append(i)
+                nums2.remove(i)
+        return l
